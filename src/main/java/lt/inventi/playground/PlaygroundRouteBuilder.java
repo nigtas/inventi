f36f724c1fcd14656d4b431d6ba77032cca7a469
nigtas <nigtas@gmail.com>
2015-05-29 14:18:45 +0300

    First commit

---
+++ src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -0,0 +1,10 @@
+package lt.inventi.playground;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class PlaygroundRouteBuilder extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+    }
+}
