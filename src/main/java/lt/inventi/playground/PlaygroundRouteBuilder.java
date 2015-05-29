b57e3852c69064f493e00e43b13c4240df0a371b
nigtas <nigtas@gmail.com>
2015-05-29 23:04:54 +0300

    2nd task done

--- src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -1,10 +1,40 @@
 package lt.inventi.playground;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.language.bean.BeanLanguage;
+
+import java.security.SecureRandom;
 
 public class PlaygroundRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        from("timer:timer?period=1000").setHeader("Number", BeanLanguage.bean(PlaygroundRouteBuilder.class, "randomGenerator(${header.firedTime})"))
+                    .choice()
+                        .when(header("Number").isGreaterThan(800)).bean(PlaygroundRouteBuilder.class, "high(${header.Number})")
+                        .when(header("Number").isLessThan(200)).bean(PlaygroundRouteBuilder.class, "low(${header.Number})")
+                        .otherwise().bean(PlaygroundRouteBuilder.class, "other(${header.Number})");
+
+    }
+
+    public void print(String sr) {
+        System.out.println(sr);
+    }
+
+    public void high(String nr) {
+        System.out.println("Number is high!! " + nr);
+    }
+
+    public void low(String nr) {
+        System.out.println("Number is low!! " + nr);
+    }
+
+    public void other(String nr) {
+        System.out.println("Number is interesting!! " + nr);
+    }
+
+    public int randomGenerator(String str) {
+        SecureRandom randomizer = new SecureRandom(str.getBytes());
+        return randomizer.nextInt(1000);
     }
 }
