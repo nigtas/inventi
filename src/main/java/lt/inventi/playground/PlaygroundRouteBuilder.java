d6dc0d1cd1b26a6ee473ee9fab4004de1b91d86a
nigtas <nigtas@gmail.com>
2015-05-30 15:59:03 +0300

    3rd task done

--- src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -1,10 +1,27 @@
 package lt.inventi.playground;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.io.FilenameUtils;
+
 
 public class PlaygroundRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        from("file://src/main/resources/?move=.done")
+                .choice()
+                    .when(body().isNotEqualTo(""))
+                        .process(new Processor() {
+                            public void process(Exchange msg) {
+                                String text = msg.getIn().getBody(String.class).toLowerCase()+ "\nEdited by Vyke";
+                                String fileName = FilenameUtils.removeExtension(msg.getIn().getHeader("CamelFileName").toString());
+                                msg.getOut().setBody(text);
+                                msg.getOut().setHeader(Exchange.FILE_NAME, fileName + "-result.txt");
+                            }
+                        })
+                            .to("file://src/main/resources/results")
+                .endChoice();
     }
 }
